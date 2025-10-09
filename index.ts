edba10d9d1a6662460b604b3bac6c9b359b5d875
tseHaoSu <153491122+tseHaoSu@users.noreply.github.com>
2025-10-09 16:27:16 +1100

    combine both scripts

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+concurrently(
+  [
+    {
+      name: "server",
+      command: "bun run dev",
+      cwd: "packages/server",
+      prefixColor: "blue",
+    },
+    {
+      name: "client",
+      command: "bun run dev",
+      cwd: "packages/client",
+      prefixColor: "green",
+    },
+  ],
+  {
+    killOthers: ["failure"],
+    restartTries: 3,
+  }
+);
